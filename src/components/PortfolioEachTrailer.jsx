93c3f557746453b49e4216026b934a04e85031c5
AndresDrimer <andresdrimer@hotmail.com>
2023-03-10 14:11:25 -0300

    eachvideo whole page

--- src/components/PortfolioEachTrailer.jsx
+++ src/components/PortfolioEachTrailer.jsx
@@ -25,9 +25,10 @@ export default function PortfolioEachTrailer(props) {
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowFullScreen
     ></iframe>
+    <div className='--portfolio-eachvideo-text-container'>
     <h4>{props.portfolio[id].title}</h4>
     <p className="--portfolio-subtitle">{props.portfolio[id].subtitle}</p>
-    <p className="--portfolio-description">{props.portfolio[id].description}</p>
+    <p className="--portfolio-description">{props.portfolio[id].description}</p></div>
   </div>
    </>
   )
