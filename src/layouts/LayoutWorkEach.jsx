3f75fa394ad9da9796c6ff0cfb99ca6946d92d3b
AndresDrimer <andresdrimer@hotmail.com>
2023-03-19 00:00:07 -0300

    some fixings css

--- src/layouts/LayoutWorkEach.jsx
+++ src/layouts/LayoutWorkEach.jsx
@@ -1,10 +1,10 @@
-
 import { useParams, useNavigate, NavLink, Link } from "react-router-dom";
 import next from "/next.svg";
+import mask from '/mask.svg'
 export default function PortfolioWorkEach(props) {
   let { id } = useParams();
   const navigate = useNavigate();
-  const numOfWorks= []
+  const numOfWorks = [];
 
   return (
     <>
@@ -12,13 +12,17 @@ export default function PortfolioWorkEach(props) {
         <div className="--portfolio-each-trailer-header">
           <h2 className="--portfolio-title">Works</h2>
         </div>
-<button
+        <button
           className="--portfolio-go-back-btn"
           onClick={() => navigate(`/`)}
-        >X</button>
-        
-        <h2 className="--eachworks-title">{props.works[id].title.toUpperCase()}</h2>
-        
+        >
+          X
+        </button>
+
+        <h2 className="--eachworks-title">
+          {props.works[id].title.toUpperCase()}
+        </h2>
+
         <iframe
           className="--portfolio-each-trailer-video"
           src={props.works[id].trailerUrl}
@@ -28,24 +32,33 @@ export default function PortfolioWorkEach(props) {
           allowFullScreen
         ></iframe>
 
-
-        <div
-          className="--portfolio-each-video-sliders-and-text"
-        >
-        
-          <button  className="--each-portfolio-btn-navigation" onClick={() => navigate(`/works/${ id === "0" ? "1" : "0"}`)}><img src={next} width="50" className="arrow-other-side" /></button>
-          <div className="--portfolio-eachvideo-text-container">
-            <h4>{props.works[id].title}</h4>
-            <p className="--portfolio-subtitle">
-              {props.works[id].subtitle}
-            </p><p className="--works-paragraph">{props.works[id].description}</p>
-            <p className="--eachworks-criticText">
-              {props.works[id].criticText}
-            </p>
-            
-          </div><button className="--each-portfolio-btn-navigation" onClick={() => navigate(`/works/${ id === "0" ? "1" : "0"}`)}><img src={next} width="50"  /></button>
+        <div className="--eachwork-each-video-sliders-and-text">
+          <div className="--eachwork-header-article">
+            <button
+              className="--each-portfolio-btn-navigation"
+              onClick={() => navigate(`/works/${id === "0" ? "1" : "0"}`)}
+            >
+              <img src={next} width="50" className="arrow-other-side" />
+            </button>
+            <div className="--eachwork-eachvideo-text-container">
+              <h4>{props.works[id].title}</h4>
+              <p className="--portfolio-subtitle">{props.works[id].subtitle}</p>
+             
+            </div>
+            <button
+              className="--each-portfolio-btn-navigation"
+              onClick={() => navigate(`/works/${id === "0" ? "1" : "0"}`)}
+            >
+              <img src={next} width="50" />
+            </button>
+          </div>
         </div>
-        
+        <div className="--eachwork-description">
+        <p className="--works-paragraph">{props.works[id].description}</p>
+        <img src={mask} alt="" width="25" className="--eachwork-mask-icon" />
+        <p className="--eachworks-criticText">
+          {props.works[id].criticText}
+        </p></div>
       </div>
     </>
   );
